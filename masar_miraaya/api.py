10f88f4acce72ddcc4247264a730a52c05d7de9d
karamakcsc <karamakcsc@hotmail.com>
2024-07-29 12:02:44 +0300

    update

--- masar_miraaya/api.py
+++ masar_miraaya/api.py
@@ -1,19 +1,19 @@
-from __future__ import unicode_literals
-import frappe, erpnext
-from frappe.utils import flt, cstr, nowdate, comma_and
-from frappe import throw, msgprint, _
-from frappe.custom.doctype.custom_field.custom_field import create_custom_field
-import requests , json
+# from __future__ import unicode_literals
+import frappe
+# from frappe.utils import flt, cstr, nowdate, comma_and
+# from frappe import throw, msgprint, _
+# from frappe.custom.doctype.custom_field.custom_field import create_custom_field
+# import requests , json
 
 @frappe.whitelist()
-def get_item_details(item=None):
+def get_item_details():
 	return frappe.db.sql("""  SELECT *
 							FROM `tabItem` ti
 							WHERE ti.disabled= 0
 							ORDER BY ti.creation DESC; """, as_dict=1)
 
 @frappe.whitelist()
-def get_wh_details(item=None):
+def get_wh_details():
 	return frappe.db.sql("""  SELECT *
 							FROM `tabWarehouse` tw
 							WHERE tw.is_group= 0
