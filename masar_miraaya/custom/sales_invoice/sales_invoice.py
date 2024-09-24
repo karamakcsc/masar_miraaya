0ff9f872b5f34b394c6cb33add9f041b25c030b6
karamakcsc <karamakcsc@hotmail.com>
2024-09-24 17:18:25 +0300

    update

--- masar_miraaya/custom/sales_invoice/sales_invoice.py
+++ masar_miraaya/custom/sales_invoice/sales_invoice.py
@@ -35,7 +35,7 @@ def make_gl(self):
                         LEFT JOIN `tabParty Account` tpa  ON tpa.parent =tc.name
                         LEFT JOIN `tabParty Account` tpa2 ON tpa2.parent = tc.customer_group
                         LEFT JOIN tabCompany tc2 ON tpa2.company = tc2.name
-                        WHERE tc.name = %s AND tc.custom_is_payment_channel = 1
+                        WHERE tc.name = %s 
             """, (row.channel_name), as_dict = True)
         if len(account) != 0:
             debit_account = (account[0]['customer_account'] or 
